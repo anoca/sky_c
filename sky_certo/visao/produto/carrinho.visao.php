dc54feb98a893b13eee96e8d032293c746f76e57
anoca <42317324+anoca@users.noreply.github.com>
2018-10-23 11:14:52 -0300

    Update carrinho.visao.php
    visaocarrinhoalterado

--- sky_certo/visao/produto/carrinho.visao.php
+++ sky_certo/visao/produto/carrinho.visao.php
@@ -3,21 +3,24 @@
 <table class="table">
     <thead>
         <tr>
-            <th>ID</th>
             <th>NOME</th>
             <th>QUANTIDADE</th>
             <th>DELETE</th>
+            <th>PRECO</th>
         </tr>
     </thead>
     <?php foreach ($carrinho as $produto): ?>
     <tr>
-        <td><?=$produto['idProduto']?></td>
         <td><?=$produto['nome']?></td>
-        <td><?=$produto['quantidade']?></td>
+        <td><input type="number" value="<?=$produto['quantidade']?>"></td>
         <td><a href="./produto/deletarCarrinho/<?=$produto['idProduto']?>" class="btn btn-danger">del</a></td>
+        <td><?=$produto['preco']?></td>
     </tr>
     <?php endforeach; ?>
 </table>
+
+<?php echo "O valor total eh de " . $totalCarrinho; ?>
+
 
 
 <a href="./produto" class="btn btn-primary">Adicionar novo produto</a>
